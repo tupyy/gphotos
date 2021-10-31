0894f8812bf94bfe1262b72666649246429c343b
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2021-10-31 16:56:29 +0100

    fix css for photo view on mobile

--- assets/js/album/gallery.js
+++ assets/js/album/gallery.js
@@ -3,15 +3,15 @@ $(function () {
       document.getElementById("album-gallery"),
       {
         autoplayFirstVideo: false,
-        pager: false,
-        galleryId: "nature",
+        pager: true,
+        galleryId: "photos",
         plugins: [lgThumbnail],
         licenseKey: '0000-0000-000-0000',
         mobileSettings: {
-          controls: false,
-          showCloseIcon: false,
-          download: false,
-          rotate: false
+          controls: true,
+          showCloseIcon: true,
+          download: true,
+          rotate: true
         }
       }
     );
