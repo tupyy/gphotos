c5337a3cbc87fdb532e466d41a65e1cfb491e666
Cosmin Tupangiu <cosmin.tupangiu@gmail.com>
2021-11-07 11:15:50 +0100

    use edit script only with edit permissions

---
+++ assets/js/album/gallery-without-edit.js
@@ -0,0 +1,20 @@
+$(function () {
+    return window.lightGallery(
+        document.getElementById("album-gallery"),
+        {
+            autoplayFirstVideo: false,
+            pager: true,
+            galleryId: "photos",
+            plugins: [lgThumbnail],
+            licenseKey: '0000-0000-000-0000',
+            mobileSettings: {
+                controls: true,
+                showCloseIcon: true,
+                download: true,
+                rotate: true
+            }
+        }
+    );
+});
+
+
