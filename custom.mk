a1df8177c457a530819742e02729801698cbbf6a
Cosmin Tupangiu <tupyy@protonmail.com>
2022-03-09 10:27:40 +0100

    change host name (#24)

---
+++ custom.mk
@@ -0,0 +1,15 @@
+KEY_FILE=$(HOME)/key.bin
+
+decrypt_conf:
+	openssl enc -aes-256-cbc -d -md sha512 -pbkdf2 -iter 10000 -in resources/.gophoto-prod.enc -out resources/.gophoto-prod.yaml -pass file:$(KEY_FILE)
+
+decrypt_realm:
+	openssl enc -aes-256-cbc -d -md sha512 -pbkdf2 -iter 10000 -in ./gophoto-realm-prod.enc -out ./gophoto-realm-prod.json -pass file:$(KEY_FILE)
+
+encrypt_conf:
+	openssl enc -aes-256-cbc -md sha512 -pbkdf2 -iter 10000 -in resources/.gophoto-prod.yaml -out resources/.gophoto-prod.enc -pass file:$(KEY_FILE) && rm resources/.gophoto-prod.yaml
+
+encrypt_realm:
+	openssl enc -aes-256-cbc -md sha512 -pbkdf2 -iter 10000 -in ./gophoto-realm-prod.enc -out ./gophoto-realm-prod.json -pass file:$(KEY_FILE)
+
+
