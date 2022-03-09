e39466e5ecbee2cd4d1bd25abcf68020b34ceb4f
Cosmin Tupangiu <tupyy@protonmail.com>
2022-03-09 12:24:36 +0100

    mod hostname

--- custom.mk
+++ custom.mk
@@ -4,12 +4,12 @@ decrypt_conf:
 	openssl enc -aes-256-cbc -d -md sha512 -pbkdf2 -iter 10000 -in resources/.gophoto-prod.enc -out resources/.gophoto-prod.yaml -pass file:$(KEY_FILE)
 
 decrypt_realm:
-	openssl enc -aes-256-cbc -d -md sha512 -pbkdf2 -iter 10000 -in ./gophoto-realm-prod.enc -out ./gophoto-realm-prod.json -pass file:$(KEY_FILE)
+	openssl enc -aes-256-cbc -d -md sha512 -pbkdf2 -iter 10000 -in ./resources/keycloak/gophoto-realm-prod.enc -out ./resources/keycloak/gophoto-realm-prod.json -pass file:$(KEY_FILE)
 
 encrypt_conf:
 	openssl enc -aes-256-cbc -md sha512 -pbkdf2 -iter 10000 -in resources/.gophoto-prod.yaml -out resources/.gophoto-prod.enc -pass file:$(KEY_FILE) && rm resources/.gophoto-prod.yaml
 
 encrypt_realm:
-	openssl enc -aes-256-cbc -md sha512 -pbkdf2 -iter 10000 -in ./gophoto-realm-prod.enc -out ./gophoto-realm-prod.json -pass file:$(KEY_FILE)
+	openssl enc -aes-256-cbc -md sha512 -pbkdf2 -iter 10000 -in ./resources/keycloak/gophoto-realm-prod.json -out ./resources/keycloak/gophoto-realm-prod.enc -pass file:$(KEY_FILE) && rm resources/keycloak/gophoto-realm-prod.json
 
 
